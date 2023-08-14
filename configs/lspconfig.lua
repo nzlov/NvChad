a7f12af7f144b1c142c15b3b41613ed77c5d851e
wfhtqp <wfhtqp@gmail.com>
2023-08-14 22:03:02 +0800

    feat: go lsp enabled inlay hint

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,7 +1,25 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.completionProvider then
+      vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+    end
+    if client.server_capabilities.definitionProvider then
+      vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
+    end
+    -- Enable inlay hints if the client supports it.
+    if client.supports_method "textDocument/inlayHint" then
+      vim.lsp.inlay_hint(bufnr, true)
+    end
+  end,
+})
+
 local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 
 local servers = {
   "html",
@@ -12,7 +30,6 @@ local servers = {
   "jsonls",
   "volar",
   "graphql",
-  "gopls",
   "rust_analyzer",
 }
 
@@ -25,3 +42,27 @@ for _, lsp in ipairs(servers) do
     },
   }
 end
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls", "serve" },
+  filetypes = { "go", "gomod" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}
